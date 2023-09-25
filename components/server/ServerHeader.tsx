76c99e0577112e4bebe8cc60d825e1a3f1f7ea07
DragosMoro <morozan.dragos@gmail.com>
2023-09-25 13:30:58 +0300

    added search function

--- components/server/ServerHeader.tsx
+++ components/server/ServerHeader.tsx
@@ -60,21 +60,21 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem className=" px-3 py-2 teext-sm cursor-pointer">
-            Create Channels
+          <DropdownMenuItem  onClick={()=>onOpen("createChannel")} className=" px-3 py-2 teext-sm cursor-pointer">
+            Create Channel
             <PlusCircle className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
         {isModerator && <DropdownMenuSeparator />}
         {isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 teext-sm cursor-pointer">
+          <DropdownMenuItem onClick={()=>onOpen("deleteServer", {server})} className="text-rose-500 px-3 py-2 teext-sm cursor-pointer">
             Delete Server
             <Trash className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
 
         {!isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 teext-sm cursor-pointer">
+          <DropdownMenuItem onClick={()=>onOpen("leaveServer", {server})} className="text-rose-500 px-3 py-2 teext-sm cursor-pointer">
             Leave Server
             <LogOut className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
