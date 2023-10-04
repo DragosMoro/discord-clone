d6557a0f9d01cbd827d2b0e3ed65b3ea0286928c
DragosMoro <morozan.dragos@gmail.com>
2023-10-04 15:17:48 +0300

    added the chat logic and implemented messages send

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import ChatHeader from "@/components/chat/ChatHeader";
 import {ChatInput} from "@/components/chat/ChatInput";
+import ChatMessages from "@/components/chat/ChatMessages";
 import { currentProfile } from "@/lib/currentProfile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -40,7 +41,8 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         serverId={params.serverId}
         type="channel"
       />
-      <div className="flex-1">Future Messages</div>
+      <ChatMessages member={member} name = {channel.name} chatId={channel.id} type="channel" apiUrl="/api/messages" socketUrl="/api/socket/messages" socketQuery={{channelId:channel.id,
+      serverId:channel.serverId}} paramKey="channelId" paramValue={channel.id}/>
       <ChatInput name={channel.name} type="channel" apiUrl="/api/socket/messages" query={{channelId:channel.id, serverId:channel.serverId}}/>
     </div>
   );
--- components/chat/ChatInput.tsx
+++ components/chat/ChatInput.tsx
@@ -8,8 +8,10 @@ import qs from "query-string";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 
 import { Input } from "@/components/ui/input";
-import { Plus, Smile } from "lucide-react";
+import { Plus} from "lucide-react";
 import { useModal } from "@/hooks/useModalStore";
+import EmojiPicker from "../EmojiPicker";
+import { useRouter } from "next/navigation";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -25,6 +27,8 @@ const formSchema = z.object({
 export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
   
   const { onOpen } = useModal();
+  const router = useRouter();
+
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -42,6 +46,8 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
         query: query,
       });
       await axios.post(url, values);
+      form.reset();
+      router.refresh();
     } catch (error) {
       console.log(error);
     }
@@ -75,7 +81,7 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                     disabled={isLoading}
                   />
                   <div className="absolute top-7 right-8">
-                    <Smile />
+                    <EmojiPicker onChange={(emoji:string) => field.onChange(`${field.value} ${emoji}`)}/>
                   </div>
                 </div>
               </FormControl>
